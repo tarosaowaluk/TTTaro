dae062e3b114909ffe78a26bb021bc97a6c043fd
tarosaowaluk <37224472+tarosaowaluk@users.noreply.github.com>
2018-03-12 17:49:06 +0700

    Update memcrasheds.py

--- memcrasheds.py
+++ memcrasheds.py
@@ -31,7 +31,7 @@ SERVER_LIST = [
 ]
 
 # Destination 
-TARGET = 'http://185.25.205.82'
+TARGET = '185.25.205.82'
 
 # optional payload to set if no keys exist
 payload = requests.get('https://google.com').text
